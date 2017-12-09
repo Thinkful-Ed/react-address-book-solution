9da67c55fccab1a7818fa9dcc9c8d0747fcf49d1
Elias Mason <eliasjmason@gmail.com>
2017-12-08 21:56:18 -0800

    No more unnecessary use of ul; some css cleanup

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -1,41 +1,39 @@
 import React from 'react';
 
-import './address-book.css';
-
 import Contact from './contact';
 
 export default class AddressBook extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            contacts: [{
-                name: 'Alice Aardvark',
-                photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
-                address: '1600 Pennsylvania Ave'
-            }, {
-                name: 'Bob Bear',
-                photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
-                address: '725 5th Ave'
-            }, {
-                name: 'Carol Coyote',
-                photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
-                address: '4 Pennsylvania Plaza'
-            }]
+            contacts: [
+                {
+                    name: 'Alice Aardvark',
+                    photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
+                    address: '1600 Pennsylvania Ave'
+                },
+                {
+                    name: 'Bob Bear',
+                    photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
+                    address: '725 5th Ave'
+                },
+                {
+                    name: 'Carol Coyote',
+                    photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
+                    address: '4 Pennsylvania Plaza'
+                }
+            ]
         };
     }
 
     render() {
-        const contacts = this.state.contacts.map((contact, index) =>
-            <li className="contact-wrapper">
-                <Contact key={index} index={index} {...contact} />
-            </li>
-        );
+        const contacts = this.state.contacts.map((contact, index) => (
+            <Contact key={index} index={index} {...contact} />
+        ));
+        
         return (
             <div className="address-book">
-                <ul>
-                    {contacts}
-                </ul>
+                {contacts}
             </div>
         );
     }
--- src/components/contact.js
+++ src/components/contact.js
@@ -5,13 +5,13 @@ import './contact.css';
 export default function Contact(props) {
     return (
         <section id={`contact-${props.index}`} className="contact">
-            <img className="contact-photo" src={props.photo} alt={`${props.name}${props.name.slice(-1) === 's' ? "'" : "'s"} avatar`}  />
-            <h2 className="contact-name">
-                {props.name}
-            </h2>
-            <address className="contact-address">
-                {props.address}
-            </address>
+            <img
+                className="contact-photo"
+                src={props.photo}
+                alt={`${props.name}'s avatar`}
+            />
+            <h2 className="contact-name">{props.name}</h2>
+            <address className="contact-address">{props.address}</address>
         </section>
     );
 }
